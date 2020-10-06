0a307d1978fe78f9445ad022e066f50cbc907a20
miguelvega2203 <71564019+miguelvega2203@users.noreply.github.com>
2020-10-06 18:06:31 -0500

    alert ordenes de compra listo

---
+++ botones.js
@@ -0,0 +1,11 @@
+//enviar a logistica
+$("#btenex").click(function(){
+ 
+   Swal.fire({        
+       type: 'success',
+       title: 'Éxito',
+       text: 'su reporte a sido enviado a logistica',        
+   });
+ 
+});
+
